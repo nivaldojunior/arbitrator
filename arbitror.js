0b808afe0b2485d7c38a06200777f556014cb836
Nivaldo Junior <nivaldojunior.93@gmail.com>
2017-12-17 11:09:10 +1000

    start the idea of compare many exchanges

--- arbitror.js
+++ arbitror.js
@@ -1,12 +1,22 @@
-#!/usr/bin/env node
-
 (async () => {
-	var ccxt = require ('ccxt')
-    let acx = new ccxt.acx ()
-	let mercado = new ccxt.mercado ()
-	let acxbook = await acx.fetchOrderBook ('BTC/AUD')
-	let mercadobook = await mercado.fetchOrderBook ('BTC/BRL')
-    console.log (acxbook.bids[0])
-	console.log (mercadobook.asks[0])
-	console.log(mercadobook.asks[0][0]/acxbook.bids[0][0])
-}) ()
+    var ccxt = require('ccxt');
+
+    let ids = ['acx', 'mercado', 'btcmarkets'];
+    let currencies = ['BRL', 'AUD'];
+    let exchanges = {};
+
+    for (let id of ids) {
+        let exchange = new ccxt[id]();
+        exchanges[id] = exchange;
+        await exchange.loadMarkets();
+
+        //filter only markets have the currencies that I want
+        exchange.symbols = exchange.symbols.filter(symbol => currencies.some(currencie => symbol.includes(currencie)));
+        for (let symbol of exchange.symbols) {
+            exchange.orderbooks = await exchange.fetchOrderBook(symbol);
+        }
+    }
+    console.log(exchanges['btcmarkets'].orderbooks);
+
+    // if (symbol in exchange.markets) {}
+})();
