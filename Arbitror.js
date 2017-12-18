b8e4354ba6e374d840f8b867aee4e4c376359ed4
Nivaldo Junior <nivaldojunior.93@gmail.com>
2017-12-18 18:49:50 +1000

    fix permutate exchanges

--- Arbitror.js
+++ Arbitror.js
@@ -1,31 +1,37 @@
 "use strict";
 
 const ccxt = require('ccxt');
+const Pair = require('./Pair');
+
+const ids = ['mercado', 'btcmarkets', 'acx'];
+const currencies = ['BRL', 'AUD'];
 
 let permuteExchanges = function (exchanges) {
     let pairs = [];
 
-    for (let i in exchanges) {
-        for(let curriencie of exchanges[i].symbols){
-             for(let j in exchanges){
+    for (let i=0; i < ids.length; i++) {
+        for(let curriencie of exchanges[ids[i]].symbols){
+             for(let j=i+1; j < ids.length; j++){
                  if(i === j)
                      continue;
 
-                 if(exchanges[j].symbols.filter(item => item.includes(curriencie.slice (0, 3)))){
-                     console.log(exchanges[i].id + '->' + exchanges[j].id + '(' + curriencie + ')');
+                 let curriencieB = exchanges[ids[j]].symbols.filter(item => item.includes(curriencie.slice (0, 3)));
+
+                 if(curriencieB.length === 1){
+                     //console.log(exchanges[ids[i]].id + '->' + exchanges[ids[j]].id + '(' + curriencie + '->' + curriencieB + ')');
+                     pairs.push(new Pair(exchanges[ids[i]].getMarket(curriencie), exchanges[ids[j]].getMarket(curriencieB)));
                  }
              }
         }
     }
+
     return pairs;
 };
 
 (async function main () {
 
-    let ids = ['mercado', 'btcmarkets'];
-    let currencies = ['BRL', 'AUD'];
     let exchanges = {};
-    let pairs = {};
+    let pairs = [];
 
     for (let id of ids) {
 
